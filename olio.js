fb8b27f5d1858e472993781b4652c86bf2dca9fb
Cristian <cristian.rusconi08@gmail.com>
2026-01-15 12:45:19 +0200

    zxcxc

---
+++ olio.js
@@ -0,0 +1,57 @@
+const olio = {
+    nimi: "Jeff",
+    ika: 30,
+    kurssi: "Full Stack Web Development",
+};
+
+console.log("Nimi:", olio.nimi);
+console.log("Ikä:", olio.ika);
+console.log("Kurssi:", olio.kurssi);
+
+const autot = {
+    merkki: "Toyota",
+    malli: "Corolla",
+    vuosi: 2020,
+    vari: "Tumman Sininen",
+};
+
+console.log("Auton merkki:", autot.merkki);
+console.log("Auton malli:", autot.malli);
+console.log("Auton vuosi:", autot.vuosi);
+console.log("Auton väri:", autot.vari);
+
+const kirja = {
+    nimi: "JavaScript: The Good Parts",
+    kirjoittaja: "Douglas Crockford",
+    sivut: 176,
+    julkaistu: 2008,
+};
+
+console.log("Kirjan nimi:", kirja.nimi);
+console.log("Kirjan kirjoittaja:", kirja.kirjoittaja);
+console.log("Kirjan sivumäärä:", kirja.sivut);
+console.log("Kirjan julkaisuvuosi:", kirja.julkaistu);
+
+const kirja2 = {
+    nimi: "Eloquent JavaScript",
+    kirjoittaja: "Marijn Haverbeke",
+    sivut: 472,
+    julkaistu: 2018,
+};
+
+console.log("Kirjan nimi:", kirja2.nimi);
+console.log("Kirjan kirjoittaja:", kirja2.kirjoittaja);
+console.log("Kirjan sivumäärä:", kirja2.sivut);
+console.log("Kirjan julkaisuvuosi:", kirja2.julkaistu);
+
+const kirja3 = {
+    nimi: "You Don't Know JS",
+    kirjoittaja: "Kyle Simpson",
+    sivut: 254,
+    julkaistu: 2015,
+};
+
+console.log("Kirjan nimi:", kirja3.nimi);
+console.log("Kirjan kirjoittaja:", kirja3.kirjoittaja);
+console.log("Kirjan sivumäärä:", kirja3.sivut);
+console.log("Kirjan julkaisuvuosi:", kirja3.julkaistu);
